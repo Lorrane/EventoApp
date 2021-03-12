e07f3c905cb6cda9f295d9225fd634f4a78910ee
Lorrane Machado <lorranemachado21@gmail.com>
2021-03-12 16:02:28 -0300

    Aula 2
    Nessa aula ela criou o modelo evento o controller para o evento organizou as pastas da view e dos controllers, criou a pagina de cadastro de evento usando thymeleaf

---
+++ eventoapp/src/main/java/br/com/michelli/eventoapp/EventoController.java
@@ -0,0 +1,16 @@
+package br.com.michelli.eventoapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventoController {
+
+	@RequestMapping("/cadastrar")
+	public String form() {
+		
+		return "evento/formEvento";
+		
+	}
+	
+}
