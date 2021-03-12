5fa25aeaab56b2046834a41f9a511442244a64a5
Lorrane Machado <lorranemachado21@gmail.com>
2021-03-12 17:37:54 -0300

    Aula 3
    Foi feita a criação de dependencia no pom, do JPa e do mysql, foi criada a classe configuration para configurar a conexão de banco de dados do mysql e foi adicionado entity no modelo evento com adição de um id auto gerador, por fim confirmado a auto geração das tabelas evento e hibernate_sequence.

--- eventoapp/src/main/java/br/com/michelli/eventoapp/controller/IndexController.java
+++ eventoapp/src/main/java/br/com/michelli/eventoapp/controller/IndexController.java
@@ -1,4 +1,4 @@
-package br.com.michelli.eventoapp;
+package br.com.michelli.eventoapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- eventoapp/src/main/java/br/com/michelli/eventoapp/model/Evento.java
+++ eventoapp/src/main/java/br/com/michelli/eventoapp/model/Evento.java
@@ -1,8 +1,20 @@
 package br.com.michelli.eventoapp.model;
 
+import java.io.Serializable;
 
-public class Evento {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Evento implements Serializable{
+
+	private static final long serialVersionUID =1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) 
+	private long id;
 	private String nome;
 	private String local;
 	private String data;
@@ -11,6 +23,12 @@ public class Evento {
 	
 	public String getNome() {
 		return nome;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
